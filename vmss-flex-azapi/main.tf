ba6cb18eab3766367e1a4266ba4cdf6884d1f828
Fitzgerald Steele <fitzgeraldsteele+github@gmail.com>
2022-05-01 17:31:42 +0000

    Added spot mix sample

--- vmss-flex-azapi/main.tf
+++ vmss-flex-azapi/main.tf
@@ -26,10 +26,11 @@ resource "azapi_resource" "vmssflex-specialty" {
   type      = "Microsoft.Compute/virtualMachineScaleSets@2021-11-01"
   name      = "flex-Mseries-azapi"
   parent_id = azurerm_resource_group.rg.id
+  location = "westus3"
 
   body = jsonencode({
     sku = {
-        capacity = 2
+        capacity = 0
         name = "Standard_M8ms"
     }
 
@@ -38,6 +39,94 @@ resource "azapi_resource" "vmssflex-specialty" {
         singlePlacementGroup = true
         platformFaultDomainCount = 3
         virtualMachineProfile = {
+            osProfile = {
+                computerNamePrefix = "vm"
+                adminUsername = "azureuser"
+                adminPassword = var.ADMIN_PASSWORD
+
+            }
+            networkProfile = {
+                networkApiVersion = "2020-11-01"
+                networkInterfaceConfigurations = [
+                    {
+                        name = "nic"
+                        properties = {
+                            ipConfigurations = [
+                                {
+                                    name = "ipconfig"
+                                    properties = {
+                                        subnet = {
+                                            id = azurerm_subnet.subnet.id
+                                        }
+                                        publicIPAddressConfiguration = {
+                                            name = "pip"
+                                            sku = {
+                                                name = "Standard"
+                                                tier = "Regional"
+                                            }
+                                            properties = {
+                                                publicIPAddressVersion = "IPv4"
+                                                idleTimeoutInMinutes = 5
+                                                deleteOption = "Delete"
+                                            }
+                                            
+                                        }
+                                    }
+                                    
+                                }
+                            ]
+                        
+                        }      
+                    }
+                ]
+            }
+            storageProfile = {
+                imageReference = {
+                    publisher = "Canonical"
+                    offer = "UbuntuServer"
+                    sku = "18.04-LTS"
+                    version = "latest"
+                }
+                osDisk = {
+                    createOption = "FromImage"
+                    managedDisk = {
+                        storageAccountType = "Premium_LRS"
+                    }                    
+                }
+            }
+        }
+    }
+    zones = []
+  })
+}
+
+
+resource "azapi_resource" "vmssflex-spotmix" {
+  type      = "Microsoft.Compute/virtualMachineScaleSets@2021-11-01"
+  name      = "flex-spotmix-azapi"
+  parent_id = azurerm_resource_group.rg.id
+  location = "westus3"
+  schema_validation_enabled = false
+
+  body = jsonencode({
+    sku = {
+        capacity = 0
+        name = "Standard_F1s"
+    }
+
+    properties = {
+        orchestrationMode = "Flexible"
+        singlePlacementGroup = false
+        platformFaultDomainCount = 1
+        priorityMixPolicy = {
+            baseRegularPriorityCount = 1
+            regularPriorityPercentageAboveBase = 20
+        }
+        virtualMachineProfile = {
+            priority = "Spot"
+            billingProfile = {
+                maxPrice = -1
+            }
             osProfile = {
                 computerNamePrefix = "vm"
                 adminUsername = "azureuser"
